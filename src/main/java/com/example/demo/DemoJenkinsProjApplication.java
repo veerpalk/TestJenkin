34c83d5d698c371027eca42a6d9142eba932e261
Veerpal Kaur <veerpal.kaur@nagarro.com>
2020-07-25 22:58:23 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoJenkinsProjApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsProjApplication.class, args);
+		System.out.print("Hello to Demo of Jenkin");
+	}
+
+}
